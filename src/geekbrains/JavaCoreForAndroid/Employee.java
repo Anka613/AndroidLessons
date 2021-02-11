2560fc7741b3e8fccffd32c20ea107a7ea3ca099
annakrasavina <anka6136@gmail.com>
2021-02-11 21:37:41 +0300

    my second commit (fixed)

--- src/geekbrains/JavaCoreForAndroid/Employee.java
+++ src/geekbrains/JavaCoreForAndroid/Employee.java
@@ -5,10 +5,10 @@ public class Employee {
     String position;
     String email;
     String phone;
-    Integer salary;
+    Double salary;
     Integer age;
 
-    public Employee(String fullName, String position, String email, String phone, Integer salary, Integer age) {
+    public Employee(String fullName, String position, String email, String phone, Double salary, Integer age) {
         this.fullName = fullName;
         this.position = position;
         this.email = email;
@@ -17,7 +17,31 @@ public class Employee {
         this.age = age;
     }
 
-    public static void showInfo(String fullName, String position, String email, String phone, int salary, int age){
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    /*public static void showInfo(String fullName, String position, String email, String phone, int salary, int age){
         System.out.println("Имя сотрудника: " + fullName);
         System.out.println("Возраст: " + age);
         System.out.println("Занимаемая должность: " + position);
@@ -25,5 +49,17 @@ public class Employee {
         System.out.println("Телефон: " + phone);
         System.out.println("Email: " + email);
         System.out.println();
+    }*/
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
     }
 }
--- src/geekbrains/JavaCoreForAndroid/Main.java
+++ src/geekbrains/JavaCoreForAndroid/Main.java
@@ -4,15 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
         Employee[] employeeArray = new Employee[5];
-        employeeArray[0] = new Employee("Петров Сидр Борисович", "Директор", "sidr@gmail.com", "+7(903)1943288", 50000, 55);
-        employeeArray[1] = new Employee("Васильев Маг Леонидович", "Психиатр", "mag333@gmail.com", "+7(903)1943288", 70000, 40);
-        employeeArray[2] = new Employee("Петров Василий Сидрович", "Стажёр", "son@gmail.com", "+7(903)7132943", 13000, 23);
-        employeeArray[3] = new Employee("Яснополе Леонид Петрович", "Почтальон", "yasnoponyatno@gmail.com", "+7(903)2849912", 5000, 15);
-        employeeArray[4] = new Employee("Ожидань Елена Пилатовна", "Бухгалтер", "helena@gmail.com", "+7(903)3947327", 150000, 35);
+        employeeArray[0] = new Employee("Петров Сидр Борисович", "Директор", "sidr@gmail.com", "+7(903)1943288", 50000.00, 55);
+        employeeArray[1] = new Employee("Васильев Маг Леонидович", "Психиатр", "mag333@gmail.com", "+7(903)1943288", 70000.00, 40);
+        employeeArray[2] = new Employee("Петров Василий Сидрович", "Стажёр", "son@gmail.com", "+7(903)7132943", 13000.00, 23);
+        employeeArray[3] = new Employee("Яснополе Леонид Петрович", "Почтальон", "yasnoponyatno@gmail.com", "+7(903)2849912", 5000.00, 15);
+        employeeArray[4] = new Employee("Ожидань Елена Пилатовна", "Бухгалтер", "helena@gmail.com", "+7(903)3947327", 150000.00, 35);
 
-        for (int i = 0; i < employeeArray.length; i++) {
+        /*for (int i = 0; i < employeeArray.length; i++) {
             if (employeeArray[i].age >= 40){
-                Employee.showInfo(employeeArray[i].fullName,employeeArray[i].position,employeeArray[i].email,employeeArray[i].phone,employeeArray[i].salary,employeeArray[i].age);
+                Employee.showInfo(
+                    employeeArray[i].fullName,
+                    employeeArray[i].position,
+                    employeeArray[i].email,
+                    employeeArray[i].phone,
+                    employeeArray[i].salary,
+                    employeeArray[i].age
+                );
+            }
+        }*/
+
+        final int maxAge = 40;
+
+        for (Employee employee: employeeArray) {
+            if (employee.getAge() >= maxAge){
+                System.out.println(employee.toString());
             }
         }
     }
